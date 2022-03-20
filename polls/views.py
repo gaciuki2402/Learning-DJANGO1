9395ce8fd4cc60321df33cc52e03f77f209ebaef
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-20 18:52:52 +0300

    initial commit

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def information(request):
+    return HttpResponse("Get dhana's company info through www.dhana.co.ke")
+    
+
+# Create your views here.
